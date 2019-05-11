9c2ad4218b6c98cefa3b30a36298573d77893683
Олександр Ступін <o.stupin@ms.npu.edu.ua>
2019-05-11 05:22:23 +0300

    final version

---
+++ Stupin41PILab02/Stupin41PILab02/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Stupin41PILab02.Models
+{
+    class Note
+    {
+        public string Filename { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
